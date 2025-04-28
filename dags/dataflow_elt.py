b83646a3b5d0f6e45682ec2bcc19e04cd7944984
paul deepak <paul@kissflow.com>
2025-04-28 16:24:21 +0530

    Update Dataflow DAG

--- dags/dataflow_elt.py
+++ dags/dataflow_elt.py
@@ -11,7 +11,7 @@ default_args = {
     'project_id': 'anz-data-platform',
     'region': 'us-central1',
     'location': 'us-central1'
-}
+}     
 
 with DAG('dataflow_elt',
           schedule_interval='@daily',
@@ -20,9 +20,9 @@ with DAG('dataflow_elt',
 
     process_transactions = DataflowStartFlexTemplateOperator(
         task_id="process_transactions",
-        template_file_gcs_path="gs://dataflow-templates/latest/flex/Python_Parquet_To_BigQuery",
         body={
-            "launch_flex_template": {
+            "launchParameter": {
+                "containerSpecGcsPath": "gs://dataflow-templates/latest/flex/Python_Parquet_To_BigQuery",
                 "parameters": {
                     "input": "gs://anz-raw-data-anz-data-platform/transactions/transactions.parquet/transaction_date={{ ds }}",
                     "output": "anz_analytics.fact_transactions"
@@ -30,12 +30,11 @@ with DAG('dataflow_elt',
                 "environment": {
                     "numWorkers": 2,
                     "maxWorkers": 10,
-		    "tempLocation": "gs://anz-dataflow-temp/tmp/"
+                    "tempLocation": "gs://anz-dataflow-temp/tmp/"
                 },
-	        "projectId": "{{ dag_run.conf.get('project_id', params['project_id']) }}",
-        	"region": "{{ params['region'] }}"
-	   }
-	},
-	location="us-central1"
+                "projectId": "{{ dag_run.conf.get('project_id', params['project_id']) }}",
+                "location": "{{ params['region'] }}"
+            }
+        },
+        location="us-central1"
     )
-
