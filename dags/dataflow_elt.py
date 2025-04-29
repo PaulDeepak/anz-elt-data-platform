c8711f7fc0edca5a2998fa36e03ef686153c636f
paul deepak <paul@kissflow.com>
2025-04-29 23:23:02 +0530

    Update script

--- dags/dataflow_elt.py
+++ dags/dataflow_elt.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.google.cloud.operators.dataflow import DataflowStartFlexTemplateOperator
+from airflow.providers.google.cloud.sensors.dataflow import DataflowJobStatusSensor
 from datetime import datetime, timedelta
 
 default_args = {
@@ -7,34 +8,38 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2025, 4, 29),
     'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'project_id': 'anz-data-platform',
-    'region': 'us-central1',
-    'location': 'us-central1'
-}     
+    'retry_delay': timedelta(minutes=15),
+    'dataflow_default_options': {
+        'project': 'anz-data-platform',
+        'region': 'us-central1',
+        'tempLocation': 'gs://anz-dataflow-temp/tmp/',
+        'serviceAccountEmail': 'anz-dataflow-sa@anz-data-platform.iam.gserviceaccount.com'
+    }
+}
 
-with DAG('dataflow_elt',
-          schedule_interval='@daily',
-          default_args=default_args,
-          catchup=False) as dag:
+with DAG('transaction_processing',
+         schedule_interval='@daily',
+         default_args=default_args,
+         max_active_runs=1,
+         catchup=False) as dag:
 
     process_transactions = DataflowStartFlexTemplateOperator(
         task_id="process_transactions",
-        body={
-            "launchParameter": {
-                "containerSpecGcsPath": "gs://dataflow-templates/latest/flex/Python_Parquet_To_BigQuery",
-                "parameters": {
-                    "input": "gs://anz-raw-data-anz-data-platform/transactions/transactions.parquet/transaction_date={{ ds }}",
-                    "output": "anz_analytics.fact_transactions"
-                },
-                "environment": {
-                    "numWorkers": 2,
-                    "maxWorkers": 10,
-                    "tempLocation": "gs://anz-dataflow-temp/tmp/"
-                },
-                "projectId": "{{ dag_run.conf.get('project_id', params['project_id']) }}",
-                "location": "{{ params['region'] }}"
-            }
+        template="gs://dataflow-templates/latest/flex/Python_Parquet_To_BigQuery",
+        parameters={
+            "input": "gs://anz-raw-data-anz-data-platform/transactions/transactions.parquet/transaction_date={{ ds }}",
+            "output": "anz_analytics.fact_transactions${{ ds_nodash }}",
+            "window_size": "21"
         },
+        location="us-central1",
+        wait_until_finished=False  # Async operation
+    )
+
+    monitor_job = DataflowJobStatusSensor(
+        task_id="monitor_job",
+        job_id="{{ task_instance.xcom_pull(task_ids='process_transactions')['job_id'] }}",
+        expected_statuses={"JOB_STATE_DONE"},
         location="us-central1"
     )
+
+    process_transactions >> monitor_job
